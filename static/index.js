43cf12394412810cd6d0807e500d5a2e6a8604a5
Cassius W <admin@mangopi.xyz>
2025-04-24 13:30:07 +0100

    call few people friends and fewer people enemies

--- static/index.js
+++ static/index.js
@@ -1,17 +1,3 @@
-/* global Element */
-
-/**
- *  The Annoying Site
- *  https://theannoyingsite.com
- *
- *  Author:
- *    Feross Aboukhadijeh
- *    https://feross.org
- *
- *  Patreon:
- *    If you enjoyed this, please support me on Patreon!
- *    https://www.patreon.com/feross
- */
 
 const SCREEN_WIDTH = window.screen.availWidth
 const SCREEN_HEIGHT = window.screen.availHeight
@@ -52,34 +38,14 @@ const ART = [
 ]
 
 const SEARCHES = [
-  'where should i bury the body',
-  'why does my eye twitch',
-  'why is my poop green',
-  'why do i feel so empty',
-  'why do i always feel hungry',
-  'why do i always have diarrhea',
-  'why does my anus itch',
-  'why does my belly button smell',
-  'why does my cat attack me',
-  'why does my dog eat poop',
-  'why does my fart smell so bad',
-  'why does my mom hate me',
-  'why does my pee smell bad',
-  'why does my poop float',
-  'proof that the earth is flat'
+  ''
 ]
 
 const VIDEOS = [
-  'albundy.mp4',
   'badger.mp4',
   'cat.mp4',
-  'hasan.mp4',
-  'heman.mp4',
-  'jozin.mp4',
   'nyan.mp4',
   'rickroll.mp4',
-  'space.mp4',
-  'trolol.mp4'
 ]
 
 const FILE_DOWNLOADS = [
@@ -91,7 +57,6 @@ const FILE_DOWNLOADS = [
   'cat-marshmellows.jpg',
   'cat-small-face.jpg',
   'cat-smirk.jpg',
-  'patreon.png'
 ]
 
 const PHRASES = [
@@ -106,43 +71,7 @@ const PHRASES = [
 ]
 
 const LOGOUT_SITES = {
-  AOL: ['GET', 'https://my.screenname.aol.com/_cqr/logout/mcLogout.psp?sitedomain=startpage.aol.com&authLev=0&lang=en&locale=us'],
-  'AOL 2': ['GET', 'https://api.screenname.aol.com/auth/logout?state=snslogout&r=' + Math.random()],
-  Amazon: ['GET', 'https://www.amazon.com/gp/flex/sign-out.html?action=sign-out'],
-  Blogger: ['GET', 'https://www.blogger.com/logout.g'],
-  Delicious: ['GET', 'https://www.delicious.com/logout'], // works!
-  DeviantART: ['POST', 'https://www.deviantart.com/users/logout'],
-  DreamHost: ['GET', 'https://panel.dreamhost.com/index.cgi?Nscmd=Nlogout'],
-  Dropbox: ['GET', 'https://www.dropbox.com/logout'],
-  eBay: ['GET', 'https://signin.ebay.com/ws/eBayISAPI.dll?SignIn'],
-  Gandi: ['GET', 'https://www.gandi.net/login/out'],
-  GitHub: ['GET', 'https://github.com/logout'],
-  GMail: ['GET', 'https://mail.google.com/mail/?logout'],
   Google: ['GET', 'https://www.google.com/accounts/Logout'], // works!
-  Hulu: ['GET', 'https://secure.hulu.com/logout'],
-  Instapaper: ['GET', 'https://www.instapaper.com/user/logout'],
-  Linode: ['GET', 'https://manager.linode.com/session/logout'],
-  LiveJournal: ['POST', 'https://www.livejournal.com/logout.bml', { 'action:killall': '1' }],
-  MySpace: ['GET', 'https://www.myspace.com/index.cfm?fuseaction=signout'],
-  NetFlix: ['GET', 'https://www.netflix.com/Logout'],
-  'New York Times': ['GET', 'https://www.nytimes.com/logout'],
-  Newegg: ['GET', 'https://secure.newegg.com/NewMyAccount/AccountLogout.aspx'],
-  Photobucket: ['GET', 'https://photobucket.com/logout'],
-  Skype: ['GET', 'https://secure.skype.com/account/logout'],
-  Slashdot: ['GET', 'https://slashdot.org/my/logout'],
-  SoundCloud: ['GET', 'https://soundcloud.com/logout'],
-  'Steam Community': ['GET', 'https://steamcommunity.com/?action=doLogout'],
-  'Steam Store': ['GET', 'https://store.steampowered.com/logout/'],
-  ThinkGeek: ['GET', 'https://www.thinkgeek.com/brain/account/login.cgi?a=lo'],
-  Threadless: ['GET', 'https://www.threadless.com/logout'],
-  Tumblr: ['GET', 'https://www.tumblr.com/logout'],
-  Vimeo: ['GET', 'https://vimeo.com/log_out'],
-  Wikipedia: ['GET', 'https://en.wikipedia.org/w/index.php?title=Special:UserLogout'],
-  'Windows Live': ['GET', 'https://login.live.com/logout.srf'],
-  Woot: ['GET', 'https://account.woot.com/logout'],
-  Wordpress: ['GET', 'https://wordpress.com/wp-login.php?action=logout'],
-  Yahoo: ['GET', 'https://login.yahoo.com/config/login?.src=fpctx&logout=1&.direct=1&.done=https://www.yahoo.com/'],
-  YouTube: ['POST', 'https://www.youtube.com', { action_logout: '1' }]
 }
 
 /**
@@ -364,7 +293,7 @@ function registerProtocolHandlers () {
   const handlerUrl = window.location.href + '/url=%s'
 
   protocolWhitelist.forEach(proto => {
-    navigator.registerProtocolHandler(proto, handlerUrl, 'The Annoying Site')
+    navigator.registerProtocolHandler(proto, handlerUrl, 'Your car keys')
   })
 }
 
@@ -401,10 +330,6 @@ function requestCameraAndMic () {
   })
 }
 
-/**
- * Animating the URL with emojis
- * See: https://matthewrayfield.com/articles/animating-urls-with-javascript-and-emojis/
- */
 function animateUrlWithEmojis () {
   if (window.ApplePaySession) {
     // Safari doesn't show the full URL anyway, so we can't animate it
@@ -412,7 +337,7 @@ function animateUrlWithEmojis () {
   }
   const rand = Math.random()
   if (rand < 0.33) {
-    animateUrlWithBabies()
+    animateUrlWithMoons()
   } else if (rand < 0.67) {
     animateUrlWithWave()
   } else {
@@ -912,7 +837,7 @@ function rainbowThemeColor () {
  * Copy cat pictures onto the user's clipboard. Requires user-initiated event.
  */
 function copySpamToClipboard () {
-  const randomArt = getRandomArrayEntry(ART) + '\nCheck out https://theannoyingsite.com'
+  const randomArt = getRandomArrayEntry(ART) + ''
   clipboardCopy(randomArt)
 }
 
